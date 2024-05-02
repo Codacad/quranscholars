5facf00ebb9c6953cbe6da40a5b80ede46aead91
Mohd Rizwan <iamrizwan40@gmail.com>
2024-05-02 12:14:56 +0300

    updates 1.1 in cart button

--- src/components/cart/CardButton.jsx
+++ src/components/cart/CardButton.jsx
@@ -3,7 +3,7 @@ import { IoCart } from "react-icons/io5";
 const CardButton = () => {
   return (
     <>
-      <div className="cart-button fixed bottom-12 right-12 flex justify-center items-center">
+      <div className="cart-button fixed md:bottom-12 md:right-12 bottom-4 right-4 flex justify-center items-center">
         <button className="w-16 h-16 bg-primary text-white transition-all duration-150 ease-in-out shadow-2xl hover:opacity-90 rounded-full flex justify-center items-center">
           <IoCart size={30} className=""/>
         </button>
